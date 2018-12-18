9d3556f9f88d4a7b5f0c8c29e899c70bdc7e99cc
Yusriyah IM <44856404+YusriyahIM@users.noreply.github.com>
2018-12-18 11:32:09 +0700

    Add files via upload

---
+++ gorengan.py
@@ -0,0 +1,7 @@
+gorengan = ["bakwan","cireng","bala-bala","gehu","combro","pisang goreng","pukis","risoles"]
+beli = "tempe"
+
+if beli in gorengan:
+    print('mamang bilang,"ya saya jual',beli,'"')
+if not beli in gorengan:
+    print('"saya gak jual',beli,'"')
